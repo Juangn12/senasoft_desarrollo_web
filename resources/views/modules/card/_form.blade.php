e1920c016b0f3db36636fcf7c2e2a355e318135c
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2021-10-21 05:10:30 -0500

    Correccion en formulario de cards

--- resources/views/modules/card/_form.blade.php
+++ resources/views/modules/card/_form.blade.php
@@ -4,14 +4,14 @@
         <input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ old('name', $card->name) }}" required autofocus />
         <div>
             <label for="photo">Foto</label>
-                <input id="photo" class="block" type="file" name="photo" />
+                <input id="photo" class="block" type="file" name="photo" required/>
         </div>
     <label for="rol">Rol</label>
         <select name="rol" id="rol" required>
             <option value="" >Seleccione</option>
             <option value="Programadores"{{ old('rol', $card->rol)== 'Programadores'? 'selected': ''}}>Programadores</option>
             <option value="Modulo" {{ old('rol', $card->rol)== 'Modulo'? 'selected': ''}}>Modulo</option>
-            <option value="TipoError" {{ old('rol', $card->rol)== 'TipoError'? 'selected': ''}}>Tipo de Error</option>
+            <option value="Tipo de Error" {{ old('rol', $card->rol)== 'Tipo de Error'? 'selected': ''}}>Tipo de Error</option>
         </select>
     <label for="state">Estado</label>
         <select name="state" id="state" required>
